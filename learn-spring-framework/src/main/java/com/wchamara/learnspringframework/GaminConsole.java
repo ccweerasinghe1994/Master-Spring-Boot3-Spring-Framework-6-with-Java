2e1080cf86d22b9a208f92f715a91bf0bcf5e02b
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-15 09:58:07 +0530

    Add GaminConsole interface

---
+++ learn-spring-framework/src/main/java/com/wchamara/learnspringframework/GaminConsole.java
@@ -0,0 +1,16 @@
+package com.wchamara.learnspringframework;
+
+/**
+ * GaminConsole
+ */
+public interface GaminConsole {
+
+    void up();
+
+    void down();
+
+    void left();
+
+    void right();
+
+}
