a3509392f2ae37a0cbad7ff2238da35d71220117
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-15 09:58:22 +0530

    Add GameRunner class

---
+++ learn-spring-framework/src/main/java/com/wchamara/learnspringframework/GameRunner.java
@@ -0,0 +1,19 @@
+package com.wchamara.learnspringframework;
+
+public class GameRunner {
+
+    private GaminConsole game;
+
+    public GameRunner(GaminConsole game) {
+        this.game = game;
+    }
+
+    public void run() {
+        System.out.println("Running the game..." + game);
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+
+}
