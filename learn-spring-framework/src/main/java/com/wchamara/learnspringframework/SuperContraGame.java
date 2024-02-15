a4a4fbb1d91e79180467d013f4199c9096f39951
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-15 09:58:45 +0530

    Add SuperContraGame class implementing GaminConsole interface

---
+++ learn-spring-framework/src/main/java/com/wchamara/learnspringframework/SuperContraGame.java
@@ -0,0 +1,20 @@
+package com.wchamara.learnspringframework;
+
+public class SuperContraGame implements GaminConsole {
+
+    public void up() {
+        System.out.println("Jumping");
+    }
+
+    public void down() {
+        System.out.println("sit down");
+    }
+
+    public void left() {
+        System.out.println("Go Back");
+    }
+
+    public void right() {
+        System.out.println("Fire a bullet");
+    }
+}
