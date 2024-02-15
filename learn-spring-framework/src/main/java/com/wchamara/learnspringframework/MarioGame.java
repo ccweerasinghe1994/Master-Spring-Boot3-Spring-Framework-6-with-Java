0bd6d54d545a3de1809d3aebcfa5a7c3f6d72779
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-15 09:58:32 +0530

    Add MarioGame class implementing GaminConsole interface

---
+++ learn-spring-framework/src/main/java/com/wchamara/learnspringframework/MarioGame.java
@@ -0,0 +1,21 @@
+package com.wchamara.learnspringframework;
+
+public class MarioGame implements GaminConsole {
+
+    public void up() {
+        System.out.println("Jumping");
+    }
+
+    public void down() {
+        System.out.println("Going down");
+    }
+
+    public void left() {
+        System.out.println("Going left");
+    }
+
+    public void right() {
+        System.out.println("Going right");
+    }
+
+}
