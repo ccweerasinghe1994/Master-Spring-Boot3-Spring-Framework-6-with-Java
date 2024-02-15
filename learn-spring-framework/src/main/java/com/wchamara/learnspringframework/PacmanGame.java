13dad19ba55e0aa0db1899b0e791214efe606fa9
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-15 09:58:38 +0530

    Add PacmanGame class implementation

---
+++ learn-spring-framework/src/main/java/com/wchamara/learnspringframework/PacmanGame.java
@@ -0,0 +1,21 @@
+package com.wchamara.learnspringframework;
+
+public class PacmanGame implements GaminConsole {
+
+    public void up() {
+        System.out.println("Going up");
+    }
+
+    public void down() {
+        System.out.println("Going down");
+    }
+
+    public void left() {
+        System.out.println("Going left");
+    }
+
+    public void right() {
+        System.out.println("Going right");
+    }
+
+}
