e72ebd84c9d9ba1ec56aa1a41adabc062ba2fafd
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-03-17 11:49:16 +0530

    006 Step 04 - 01 - Enhancing Spring MVC Controller to provide HTML response

--- myfirstwebapp/src/main/java/com/wchamara/myfirstwebapp/hello/SayHelloController.java
+++ myfirstwebapp/src/main/java/com/wchamara/myfirstwebapp/hello/SayHelloController.java
@@ -1,7 +1,6 @@
 package com.wchamara.myfirstwebapp.hello;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +10,26 @@ public class SayHelloController {
     @ResponseBody
     public String sayHello() {
         return "Hello, World!";
+    }
+
+    @RequestMapping("/say-hello-in-html")
+    @ResponseBody
+    public String sayHelloInHTML() {
+
+        StringBuffer html = new StringBuffer();
+        html.append("<html>");
+        html.append("<head>");
+        html.append("<title>Home</title>");
+        html.append("</head>");
+        html.append("<body>");
+        html.append("<h1>Home</h1>");
+        html.append("<p>Welcome to the home page updated</p>");
+        html.append("<a href=\"about.php\">About</a>");
+        html.append("<a href=\"contact.php\">Contact</a>");
+        html.append("</body>");
+        html.append("</html>");
+
+
+        return html.toString();
     }
 }
